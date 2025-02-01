ea0b6478c5038b423fafcfea965be67e439eb432
datoi <dato.tadiashvili13@gmail.com>
2025-02-01 21:57:32 +0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Weather from './components/homepage/Weather'
+
+function App() {
+
+  return (
+    <>
+    <Weather/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/homepage/Weather.jsx
@@ -0,0 +1,95 @@
+import './Weather.css'
+import { useState,useEffect } from 'react';
+const Weather = () => {
+    const [latitude, setLatitude] = useState(null);  
+    const [longitude, setLongitude] = useState(null); 
+    const [weather, setWeather] = useState(null);     
+    const [loading, setLoading] = useState(true);     
+    const [value, setValue] = useState('')
+
+    useEffect(() => {
+        const fetchWeatherData = async () => {
+            if (latitude && longitude) { 
+                try {
+                    const response = await fetch(
+                        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+                    );
+                    if (!response.ok) {
+                        throw new Error(`HTTP error: ${response.status}`);
+                    }
+                    const data = await response.json();
+                    setWeather(data.current_weather);
+                } catch (error) {
+                    console.error(error);
+                } finally {
+                    setLoading(false); 
+                }
+            }
+        };
+
+        fetchWeatherData();
+    }, [latitude, longitude]); 
+
+  
+    const getCoordinates = async (city) => {
+        const url = `https://nominatim.openstreetmap.org/search?format=json&q=${city}`;
+        
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            
+            if (data.length > 0) {
+                const lat = parseFloat(data[0].lat);
+                const lon = parseFloat(data[0].lon);
+                console.log(`City: ${city}`);
+                console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+                
+                setLatitude(lat);  
+                setLongitude(lon); 
+            } else {
+                console.log("Location not found.");
+            }
+        } catch (error) {
+            console.error("Error fetching coordinates:", error);
+        }
+    };
+    
+    const searchCity = (event) => {
+        setValue(event.target.value)
+    }
+
+    const searchClick = () => {
+            getCoordinates(value); 
+    }
+    
+    return (
+        
+        <div>
+            <div className="search-container">
+                <input
+                    onChange={searchCity}
+                    value={value}
+                    type="text"
+                    className="search-input"
+                    placeholder="Search..."
+                />
+                <button onClick={searchClick} className="search-button">Search</button> {/* Button next to the input */}
+            </div>
+
+            {loading ? (
+                <p>Pick the city</p>
+            ) : weather ? (
+                <div>
+                    <p>Time: {weather.time}</p>
+                    <p>Temperature: {weather.temperature}°C</p>
+                    <p>Wind Speed: {weather.windspeed} km/h</p>
+                    
+                </div>
+            ) : (
+                <p>No weather data available.</p>
+            )}
+        </div>
+    );
+};
+
+export default Weather;
